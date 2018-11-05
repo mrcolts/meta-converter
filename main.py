5d0075fc1da2c60bdb936cb703b4b43ac9fb6379
Mikhail Oleynikov <quaiby@icloud.com>
2018-11-05 16:59:41 +0300

    Теперь мы храним и мету, ибо заебало

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import json
-import urllib3
+
 import time
 
+import certifi
+import urllib3
+
 TIME_FORMAT = '%Y-%m-%dT%H:%M:%S+00:00'
-MODERN_TIME = time.strptime('2013-10-25T13:00:00+00:00', TIME_FORMAT)
-LEGACY_TIME = time.strptime('2011-09-18T22:00:00+00:00', TIME_FORMAT)
+MODERN_ENDPOINT = time.strptime('2013-10-25T13:00:00+00:00', TIME_FORMAT)
+LEGACY_ENDPOINT = time.strptime('2011-09-18T22:00:00+00:00', TIME_FORMAT)
 OFFICIAL_MANIFEST = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
 COMMUNITY_MANIFEST = './community_versions_manifest.json'
 OUTPUT_MANIFEST = './manifest.json'
-HEADERS = {"Host":"launchermeta.mojang.com", "User-Agent":"Mozilla/5.0"}
+OUTPUT_META = './meta/{}_{}.json'
+HEADERS = {"Host": "launchermeta.mojang.com", "User-Agent": "Mozilla/5.0"}
 
-http = urllib3.PoolManager(headers=HEADERS)
+http = urllib3.PoolManager(
+    cert_reqs='CERT_REQUIRED',
+    ca_certs=certifi.where(),
+    headers=HEADERS
+)
+
 
 def load_manifests():
     response = http.request("GET", OFFICIAL_MANIFEST)
@@ -18,8 +27,50 @@ def load_manifests():
     community = json.load(open(COMMUNITY_MANIFEST))
     return original, community
 
-def dump_manifest(manifest):
-    json.dump(manifest, open(OUTPUT_MANIFEST, "w+"))
+
+def convert_meta(url):
+    meta: dict = json.loads(http.request("GET", url).data.decode("utf-8"))
+
+    output = {
+        "client": meta["downloads"]["client"]["url"],
+        "mainClass": meta["mainClass"],
+    }
+
+    if "arguments" in meta.keys():
+        output["arguments"] = [x for x in meta["arguments"]["game"] if "rules" not in x]
+    else:
+        output["arguments"] = [x for x in meta["minecraftArguments"].split(" ")]
+
+    output["assets"] = {
+        "id": meta["assetIndex"]["id"],
+        "url": meta["assetIndex"]["url"]
+    }
+
+    libraries = []
+    natives = []
+
+    for lib in meta["libraries"]:
+        if "classifiers" in lib["downloads"]:
+            native = {}
+            for key, value in lib["natives"].items():
+                raw_native = lib["downloads"]["classifiers"][value]
+                native[key] = {
+                    "path": raw_native["path"],
+                    "url": raw_native["url"]
+                }
+            natives.append(native)
+        else:
+            raw_lib = lib["downloads"]["artifact"]
+            libraries.append({
+                "path": raw_lib["path"],
+                "url": raw_lib["url"]
+            })
+
+    output["libraries"] = libraries
+    output["natives"] = natives
+
+    json.dump(output, open(OUTPUT_META.format(meta["type"], meta["id"]), "w+"))
+
 
 def main():
     original, community = load_manifests()
@@ -29,15 +80,14 @@ def main():
     for o_entry in original["versions"]:
         release_time = time.strptime(o_entry["releaseTime"], TIME_FORMAT)
 
-        new_type = "modern" if release_time > MODERN_TIME else "legacy" if release_time > LEGACY_TIME else "classic"
+        new_type = "modern" if release_time > MODERN_ENDPOINT else "legacy" if release_time > LEGACY_ENDPOINT else "classic"
 
         manifest.append({
-            "id":       o_entry["id"],
-            "vendor":   "official",
-            "type":     new_type,
-            "subtype":  o_entry["type"],
-            "i18nKey":  f"misc.version.{new_type}.{o_entry['type']}",
-            "url":      o_entry["url"]
+            "id": o_entry["id"],
+            "vendor": "official",
+            "type": new_type,
+            "subtype": o_entry["type"],
+            "url": o_entry["url"]
         })
 
         marked_to_pop = []
@@ -50,7 +100,10 @@ def main():
         for i in marked_to_pop:
             community.pop(i)
 
-    dump_manifest(manifest)   
+        convert_meta(o_entry["url"])
+
+    json.dump(manifest, open(OUTPUT_MANIFEST, "w+"))
+
 
 if __name__ == "__main__":
     main()
